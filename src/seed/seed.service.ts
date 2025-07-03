4233971d848b7348132440c5701742f4f3ad7922
David <jdnarvaez@unimagdalena.edu.co>
2025-07-03 10:50:19 -0500

    feat/configurados los modulos para que seed pueda usar los service de cars y brans sin problemas Todo esto se hace con el fin de poblar el DB (json quemado en service) de los servicios que tenemos disponibles con seeds

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,11 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { CarsService } from '../cars/cars.service';
+import { CARS_SEED } from './data/cars.seed';
+import { BrandsService } from 'src/brands/brands.service';
+import { BRADS_SEED } from './data/brandss.seed';
 
 
 @Injectable()
 export class SeedService {
  
+  constructor(
+    private readonly carsService: CarsService,
+    private readonly brandsService: BrandsService
+  ){
+
+  }
+
 
   populateDB() {
+
+    this.carsService.fillCarsWithSeedData(CARS_SEED);
+    this.brandsService.fillBrandsWithSeedData(BRADS_SEED);
 
     return 'Seed Execute'
 
